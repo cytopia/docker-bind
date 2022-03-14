97e3f8bf58a2e62f30e1b73487974acdf7074e5f
cytopia <cytopia@everythingcli.org>
2022-03-14 16:19:38 +0100

    Repeat tests aggressively

--- tests/05-extrahosts-cname.sh
+++ tests/05-extrahosts-cname.sh
@@ -17,25 +17,27 @@ DEBUG="${6}"
 
 NAME="bind$( shuf -i 1000000000-2000000000 -n 1 )"
 PORT="5300"
-WAIT=10
+WAIT=5
+REPS=10
 
 
 # DEBUG_ENTRYPOINT=2
-run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=2 -e 'EXTRA_HOSTS=www.devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
+run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=2 -e DOCKER_LOGS=1 -e 'EXTRA_HOSTS=www.devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
 run "sleep ${WAIT}"
 sanity_check "${NAME}"
-if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-	if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-		if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-			echo "FAILED: www.devilbox is not resolvable"
-			run "dig @127.0.0.1 -p ${PORT} +short www.devilbox"
-			run "docker logs ${NAME}"
-			run "docker stop ${NAME}"
-			echo "ABORT..."
-			exit 1
-		fi
+i=0
+while ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; do
+	i=$(( i + 1 ))
+	if [ "${i}" -gt "${REPS}" ]; then
+		echo "FAILED: www.devilbox is not resolvable"
+		run "dig @127.0.0.1 -p ${PORT} +short www.devilbox"
+		run "docker logs ${NAME}"
+		run "docker stop ${NAME}"
+		echo "ABORT..."
+		exit 1
 	fi
-fi
+	sleep 1
+done
 if [ "$( dig @127.0.0.1 -p ${PORT} +short t1.devilbox | wc -l )" != "0" ]; then
 	run "docker stop ${NAME}"
 	exit 1
@@ -44,21 +46,22 @@ docker_stop "${NAME}"
 
 
 # DEBUG_ENTRYPOINT=1
-run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=1 -e 'EXTRA_HOSTS=www.devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
+run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=1 -e DOCKER_LOGS=1 -e 'EXTRA_HOSTS=www.devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
 run "sleep ${WAIT}"
 sanity_check "${NAME}"
-if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-	if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-		if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-			echo "FAILED: www.devilbox is not resolvable"
-			run "dig @127.0.0.1 -p ${PORT} +short www.devilbox"
-			run "docker logs ${NAME}"
-			run "docker stop ${NAME}"
-			echo "ABORT..."
-			exit 1
-		fi
+i=0
+while ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; do
+	i=$(( i + 1 ))
+	if [ "${i}" -gt "${REPS}" ]; then
+		echo "FAILED: www.devilbox is not resolvable"
+		run "dig @127.0.0.1 -p ${PORT} +short www.devilbox"
+		run "docker logs ${NAME}"
+		run "docker stop ${NAME}"
+		echo "ABORT..."
+		exit 1
 	fi
-fi
+	sleep 1
+done
 if [ "$( dig @127.0.0.1 -p ${PORT} +short t1.devilbox | wc -l )" != "0" ]; then
 	run "docker stop ${NAME}"
 	exit 1
@@ -67,21 +70,22 @@ docker_stop "${NAME}"
 
 
 # DEBUG_ENTRYPOINT=0
-run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=0 -e 'EXTRA_HOSTS=www.devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
+run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=0 -e DOCKER_LOGS=1 -e 'EXTRA_HOSTS=www.devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
 run "sleep ${WAIT}"
 sanity_check "${NAME}"
-if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-	if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-		if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-			echo "FAILED: www.devilbox is not resolvable"
-			run "dig @127.0.0.1 -p ${PORT} +short www.devilbox"
-			run "docker logs ${NAME}"
-			run "docker stop ${NAME}"
-			echo "ABORT..."
-			exit 1
-		fi
+i=0
+while ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; do
+	i=$(( i + 1 ))
+	if [ "${i}" -gt "${REPS}" ]; then
+		echo "FAILED: www.devilbox is not resolvable"
+		run "dig @127.0.0.1 -p ${PORT} +short www.devilbox"
+		run "docker logs ${NAME}"
+		run "docker stop ${NAME}"
+		echo "ABORT..."
+		exit 1
 	fi
-fi
+	sleep 1
+done
 if [ "$( dig @127.0.0.1 -p ${PORT} +short t1.devilbox | wc -l )" != "0" ]; then
 	run "docker stop ${NAME}"
 	exit 1
@@ -90,21 +94,22 @@ docker_stop "${NAME}"
 
 
 # DEBUG_ENTRYPOINT=null
-run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e 'EXTRA_HOSTS=www.devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
+run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DOCKER_LOGS=1 -e 'EXTRA_HOSTS=www.devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
 run "sleep ${WAIT}"
 sanity_check "${NAME}"
-if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-	if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-		if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
-			echo "FAILED: www.devilbox is not resolvable"
-			run "dig @127.0.0.1 -p ${PORT} +short www.devilbox"
-			run "docker logs ${NAME}"
-			run "docker stop ${NAME}"
-			echo "ABORT..."
-			exit 1
-		fi
+i=0
+while ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; do
+	i=$(( i + 1 ))
+	if [ "${i}" -gt "${REPS}" ]; then
+		echo "FAILED: www.devilbox is not resolvable"
+		run "dig @127.0.0.1 -p ${PORT} +short www.devilbox"
+		run "docker logs ${NAME}"
+		run "docker stop ${NAME}"
+		echo "ABORT..."
+		exit 1
 	fi
-fi
+	sleep 1
+done
 if [ "$( dig @127.0.0.1 -p ${PORT} +short t1.devilbox | wc -l )" != "0" ]; then
 	docker stop "${NAME}"
 	exit 1
