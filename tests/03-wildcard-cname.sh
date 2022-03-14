1d740b19193699976931d2dde3819aa20e32bcca
cytopia <cytopia@everythingcli.org>
2022-03-14 14:24:35 +0100

    Make tests more verbose

--- tests/03-wildcard-cname.sh
+++ tests/03-wildcard-cname.sh
@@ -17,15 +17,18 @@ DEBUG="${6}"
 
 NAME="bind$( shuf -i 1000000000-2000000000 -n 1 )"
 PORT="5300"
+WAIT=10
 
 
 # DEBUG_ENTRYPOINT=2
 run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=2 -e 'WILDCARD_DNS=devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
-run "sleep 5"
+run "sleep ${WAIT}"
 sanity_check "${NAME}"
-if [ "$( run "dig @127.0.0.1 -p ${PORT} +short www.devilbox" "0" "1" | wc -l )" = "0" ]; then
-	echo "FAILED: Not exactly one line retrieved from dig command"
+if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
+	echo "FAILED: www.devilbox is not resolvable"
+	run "docker logs ${NAME}"
 	run "docker stop ${NAME}"
+	echo "ABORT..."
 	exit 1
 fi
 docker_stop "${NAME}"
@@ -33,11 +36,13 @@ docker_stop "${NAME}"
 
 # DEBUG_ENTRYPOINT=1
 run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=1 -e 'WILDCARD_DNS=devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
-run "sleep 5"
+run "sleep ${WAIT}"
 sanity_check "${NAME}"
-if [ "$( run "dig @127.0.0.1 -p ${PORT} +short www.devilbox" "0" "1" | wc -l )" = "0" ]; then
-	echo "FAILED: Not exactly one line retrieved from dig command"
+if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
+	echo "FAILED: www.devilbox is not resolvable"
+	run "docker logs ${NAME}"
 	run "docker stop ${NAME}"
+	echo "ABORT..."
 	exit 1
 fi
 docker_stop "${NAME}"
@@ -45,11 +50,13 @@ docker_stop "${NAME}"
 
 # DEBUG_ENTRYPOINT=0
 run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=0 -e 'WILDCARD_DNS=devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
-run "sleep 5"
+run "sleep ${WAIT}"
 sanity_check "${NAME}"
-if [ "$( run "dig @127.0.0.1 -p ${PORT} +short www.devilbox" "0" "1" | wc -l )" = "0" ]; then
-	echo "FAILED: Not exactly one line retrieved from dig command"
+if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
+	echo "FAILED: www.devilbox is not resolvable"
+	run "docker logs ${NAME}"
 	run "docker stop ${NAME}"
+	echo "ABORT..."
 	exit 1
 fi
 docker_stop "${NAME}"
@@ -57,11 +64,13 @@ docker_stop "${NAME}"
 
 # DEBUG_ENTRYPOINT=null
 run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e 'WILDCARD_DNS=devilbox=google.com' -p ${PORT}:53/udp ${IMAGE}:${TAG}"
-run "sleep 5"
+run "sleep ${WAIT}"
 sanity_check "${NAME}"
-if [ "$( run "dig @127.0.0.1 -p ${PORT} +short www.devilbox" "0" "1" | wc -l )" = "0" ]; then
-	echo "FAILED: Not exactly one line retrieved from dig command"
+if ! run "dig @127.0.0.1 -p ${PORT} +short www.devilbox | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$'"; then
+	echo "FAILED: www.devilbox is not resolvable"
+	run "docker logs ${NAME}"
 	run "docker stop ${NAME}"
+	echo "ABORT..."
 	exit 1
 fi
 docker_stop "${NAME}"
--- tests/06-ttl-time.sh
+++ tests/06-ttl-time.sh
@@ -34,7 +34,8 @@ docker_stop "${NAME}"
 run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=1 -e 'EXTRA_HOSTS=www.devilbox=google.com' -e TTL_TIME=500 -p ${PORT}:53/udp ${IMAGE}:${TAG}"
 run "sleep 5"
 sanity_check "${NAME}"
-if ! run "dig @127.0.0.1 -p ${PORT} www.devilbox | grep -E '^www\.devilbox\.\s+500\s+IN\s+A'"; then
+if ! run "dig @127.0.0.1 -p ${PORT} www.devilbox" "0" "1" | grep -E '^www\.devilbox\.\s+500\s+IN\s+A'; then
+	echo "FAILED: '^www\\.devilbox\\.\\s+500\\s+IN\\s+A' expected, but not found"
 	run "docker stop ${NAME}"
 	exit 1
 fi
@@ -45,7 +46,8 @@ docker_stop "${NAME}"
 run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e DEBUG_ENTRYPOINT=0 -e 'EXTRA_HOSTS=www.devilbox=google.com' -e TTL_TIME=500 -p ${PORT}:53/udp ${IMAGE}:${TAG}"
 run "sleep 5"
 sanity_check "${NAME}"
-if ! run "dig @127.0.0.1 -p ${PORT} www.devilbox | grep -E '^www\.devilbox\.\s+500\s+IN\s+A'"; then
+if ! run "dig @127.0.0.1 -p ${PORT} www.devilbox" "0" "1" | grep -E '^www\.devilbox\.\s+500\s+IN\s+A'; then
+	echo "FAILED: '^www\\.devilbox\\.\\s+500\\s+IN\\s+A' expected, but not found"
 	run "docker stop ${NAME}"
 	exit 1
 fi
@@ -56,7 +58,8 @@ docker_stop "${NAME}"
 run "docker run -d --rm --platform ${ARCH} --name ${NAME} -e DEBUG=${DEBUG} -e 'EXTRA_HOSTS=www.devilbox=google.com' -e TTL_TIME=500 -p ${PORT}:53/udp ${IMAGE}:${TAG}"
 run "sleep 5"
 sanity_check "${NAME}"
-if ! run "dig @127.0.0.1 -p ${PORT} www.devilbox | grep -E '^www\.devilbox\.\s+500\s+IN\s+A'"; then
+if ! run "dig @127.0.0.1 -p ${PORT} www.devilbox" "0" "1" | grep -E '^www\.devilbox\.\s+500\s+IN\s+A'; then
+	echo "FAILED: '^www\\.devilbox\\.\\s+500\\s+IN\\s+A' expected, but not found"
 	run "docker stop ${NAME}"
 	exit 1
 fi
